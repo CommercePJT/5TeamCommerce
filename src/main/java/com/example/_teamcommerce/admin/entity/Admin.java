5847e017f6f59aef8c462784bddcb279b1fa21c4
tls123qwe <tls123qwe@gmail.com>
2026-07-11 08:15:06 +0900

    Admin working(1/n)

--- src/main/java/com/example/_teamcommerce/admin/entity/Admin.java
+++ src/main/java/com/example/_teamcommerce/admin/entity/Admin.java
@@ -1,4 +1,35 @@
 package com.example._teamcommerce.admin.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.core.SpringVersion;
+
+@Getter
+@Entity
+@Table(name = "admins")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
 public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "이름은 필수 입력값입니다.")
+    private String name;
+
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
+    @Email
+    private String email;
+
+    @Pattern(regexp = "^010-\\d{4}-\\d{4}$")
+    private String phone;
+
+    private String status;
+
 }
--- src/main/java/com/example/_teamcommerce/auth/service/AuthService.java
+++ src/main/java/com/example/_teamcommerce/auth/service/AuthService.java
@@ -1,4 +1,33 @@
 package com.example._teamcommerce.auth.service;
 
+import com.example._teamcommerce.admin.entity.Admin;
+import com.example._teamcommerce.admin.repository.AdminRepository;
+import com.example._teamcommerce.auth.dto.request.SignupAdminRequest;
+import com.example._teamcommerce.auth.dto.response.SignupAdminResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+
 public class AuthService {
+
+    // 회원가입 = admin 생성
+    private final AdminRepository adminRepository;
+
+    // 회원가입
+    @Transactional
+    public SignupAdminResponse signup(SignupAdminRequest request) {
+
+        // 이메일 중복확인
+        if (adminRepository.existsByEmail(request.getEmail())) {
+            throw new IllegalArgumentException("다른 관리자가 사용 중인 이메일 입니다.");
+        }
+
+        // admin 객체 생성
+        Admin admin = new Admin()
+    }
+
+
 }
