0749781056c07282c13ff30b97bdb39c3eb057d1
최정윤 <jes2ngyun@gmail.com>
2026-07-12 15:52:03 +0900

    "feat: add JPA auditing base entity"

---
+++ src/main/java/com/example/_teamcommerce/common/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.example._teamcommerce.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
