c4c33576c367f9169549b4482e847f97ba0db75c
최정윤 <jes2ngyun@gmail.com>
2026-07-12 16:22:47 +0900

    "feat: add admin role and status enums"

---
+++ src/main/java/com/example/_teamcommerce/admin/type/AdminRole.java
@@ -0,0 +1,8 @@
+package com.example._teamcommerce.admin.type;
+
+public enum AdminRole {
+
+    SUPER_ADMIN,
+    OPERATION_ADMIN,
+    CS_ADMIN
+}
---
+++ src/main/java/com/example/_teamcommerce/admin/type/AdminStatus.java
@@ -0,0 +1,10 @@
+package com.example._teamcommerce.admin.type;
+
+public enum AdminStatus {
+
+    PENDING,
+    ACTIVE,
+    INACTIVE,
+    SUSPENDED,
+    REJECTED
+}
